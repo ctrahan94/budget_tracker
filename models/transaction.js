80f290b85587d5f78ddcc7fb8e5a62c9238afefa
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-11-23 10:15:32 -0700

    Working on getting atlas to work

--- models/transaction.js
+++ models/transaction.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
+const TransactionSchema = new Schema(
   {
     name: {
       type: String,
@@ -20,6 +20,6 @@ const transactionSchema = new Schema(
   }
 );
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Transaction = mongoose.model("Transaction", TransactionSchema);
 
 module.exports = Transaction;
